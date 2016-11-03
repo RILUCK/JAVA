4588f2923d190ef035ccaa7a84abbd0fae8675f2
Rishabh <rishabh.shrivas@nitdelhi.ac.in>
2016-11-03 12:16:20 +0530

    Update Solution.java

--- Java-Output-Formatting/Solution.java
+++ Java-Output-Formatting/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
     public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
             System.out.println("================================");
-            for(int i=0;i<3;i++)
+            for(int i=0;i<3;i++) //for(;cin.hasNext();)
             {
                 String s=sc.next();
                 int x=sc.nextInt();
